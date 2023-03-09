a72f46ad1d4e5092efcc6af44937140415942de6
Nguyễn Huy <boy.handsomer.37@gmail.com>
2023-03-09 11:38:37 +0700

    Add UI Picture
    Add UI Picture

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -84,3 +84,57 @@ export const ImageSwiper = styled.img({
 	height: "100%",
 	objectFit: "contain"
 });
+export const DescStunning = styled.div({
+	paddingTop: 150,
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center"
+});
+export const BoxDesc = styled.div({
+	width: "80%",
+	height: "100%",
+	display: "flex",
+	flexDirection: "row"
+});
+export const DescLeft = styled.div({
+	flex: 1
+});
+export const DescRight = styled.div({
+	flex: 1
+});
+export const ImageDesc = styled.img({
+	width: "456px",
+	height: "345px",
+	objectFit: "contain"
+});
+export const WrapperPicture = styled.div({
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	width: "100%",
+	padding: "96px 0",
+	flexDirection: "column"
+});
+
+export const ListPicture = styled.div({
+	display: "grid",
+	gridTemplateColumns: "1fr 1fr 1fr 1fr",
+	width: "80%",
+	margin: "0 auto",
+	justifyContent: "center",
+	marginTop: "24px",
+	flexDirection: "row",
+	columnGap: 20
+});
+
+export const PictureItem = styled.div({
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	flexDirection: "column"
+});
+export const ImagePictureItem = styled.img({
+	maxWidth: "100%",
+	height: "auto",
+	borderRadius: 16
+});
